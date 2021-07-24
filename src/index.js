a0b701d778563750301412614b885b0811371e67
Brylle Mutia <bryllemutia3@gmail.com>
2021-07-24 09:41:04 +0800

    updated base url for strapi api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:1337";
-axios.defaults.baseURL = "https://linearfence.herokuapp.com";
+axios.defaults.baseURL = "https://linear-fence.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
